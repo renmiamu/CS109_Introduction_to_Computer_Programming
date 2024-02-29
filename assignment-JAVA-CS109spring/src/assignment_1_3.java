bf16f82f2ba660928bd7e61bd75d3a0eb9cac52f
Hongli SHEN <149307453+renmiamu@users.noreply.github.com>
2024-02-29 18:53:22 +0800

    Create assignment_1_3.java
    too complex. Write it tomorrow.

---
+++ assignment-JAVA-CS109spring/src/assignment_1_3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class assignment_1_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int cishu = input.nextInt();//测试数据数
+        int order = 0;
+        while (cishu-- >0){
+            int start_year = input.nextInt();
+            int start_month = input.nextInt();
+            int start_day = input.nextInt();
+            int end_year = input.nextInt();
+            int end_month = input.nextInt();
+            int end_day = input.nextInt();
+            int extra_time = 0;
+            int normal = 0; //一般情况的天数
+            if (start_month % 2 ==1 ){   //开始月份为奇数月
+            if (start_year%13==0){  //检测开始年份是不是闰年//要检测两个年份中间有没有闰年
+
+            }
+            }
+            }
+        }
+
