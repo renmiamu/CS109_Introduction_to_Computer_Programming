f686f0d5aa72787f8b2f5868e7b04d8021967301
Hongli SHEN <149307453+renmiamu@users.noreply.github.com>
2024-03-18 16:56:38 +0800

    Update assignment_2_3.java
    finally！！！

--- assignment-JAVA-CS109spring/src/assignment_2_3.java
+++ assignment-JAVA-CS109spring/src/assignment_2_3.java
@@ -9,20 +9,22 @@ public class assignment_2_3 {
         int[][] array_big = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int[] array_small = new int[n];
-                array_small[j] = input.nextInt();
-                array_big[i] = array_small;
+                array_big[i][j] = input.nextInt();
             }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (array_big[i][j] == 1 && i == 0){
-                    array_big[i + 1][j] = 1;}
-                else if (array_big[i][j] == 1 && i == m - 1){
-                    array_big[i - 1][j] = 1;}
-                else if (array_big[i][j] == 1) {
-                    array_big[i - 1][j] = 1;
-                    array_big[i + 1][j] = 1;
+                if (array_big[i][j] == 1 && i == 0 && array_big[i + 1][j] != 1) {
+                    array_big[i + 1][j] = 2;
+                } else if (array_big[i][j] == 1 && i == m - 1 && array_big[i - 1][j] != 1) {
+                    array_big[i - 1][j] = 2;
+                } else if (array_big[i][j] == 1 && i > 0 && i < m - 1) {
+                    if (array_big[i - 1][j] != 1) {
+                        array_big[i - 1][j] = 2;
+                    }
+                    if (array_big[i + 1][j] != 1) {
+                        array_big[i + 1][j] = 2;
+                    }
                 }
             }
         }
@@ -30,27 +32,27 @@ public class assignment_2_3 {
         int jishu = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (array_big[j][i]==1) {
-                    if (jishu%2==0)
-                        available+=jishu/2;
+                if (array_big[j][i] == 1 || array_big[j][i] == 2) {
+                    if (jishu % 2 == 0)
+                        available += jishu / 2;
                     else
-                        available+=(jishu+1)/2;
+                        available += (jishu + 1) / 2;
                     jishu = 0;
                 }
-                if (array_big[j][i]==0)
-                    jishu+=1;
-                if (j == m-1) {
-                    if (jishu%2==0)
-                        available+=jishu/2;
+                if (array_big[j][i] == 0)
+                    jishu += 1;
+                if (j == m - 1) {
+                    if (jishu % 2 == 0)
+                        available += jishu / 2;
                     else
-                        available+=(jishu+1)/2;
+                        available += (jishu + 1) / 2;
                     jishu = 0;
                 }
             }
-            }
+        }
         if (available >= balloon_number)
             System.out.print("True");
         else
             System.out.print("False");
-        }
     }
+}
